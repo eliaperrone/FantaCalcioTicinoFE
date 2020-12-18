518ed23824294de0dd2d271b0253befa5c2a8392
elia.perrone@corner.ch <eliaperrone@MacBook-Pro-di-Elia.local>
2020-12-18 22:58:21 +0100

    update project

---
+++ FantaCalcioTicino/src/app/page/gestione/gestione.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ftc-gestione',
+  templateUrl: './gestione.component.html',
+  styleUrls: ['./gestione.component.scss']
+})
+export class GestioneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
